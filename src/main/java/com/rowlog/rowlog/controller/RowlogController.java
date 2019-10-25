2bc02933dd99533f7eb3329d47b7c9058d9391a1
bcart123 <bcarthel@aggienetwork.com>
2019-10-25 00:51:36 -0500

    added controller

---
+++ src/main/java/com/rowlog/rowlog/controller/RowlogController.java
@@ -0,0 +1,10 @@
+package com.rowlog.rowlog.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RowlogController {
+    public RowlogController() {
+
+    }
+}
