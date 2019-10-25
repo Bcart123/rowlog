134249aeacb78a948e3f1629186f0aeb3f1ef8dc
bcart123 <bcarthel@aggienetwork.com>
2019-10-25 00:38:15 -0500

    adding intial spring boot app

---
+++ src/test/java/com/rowlog/rowlog/RowlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowlog.rowlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
