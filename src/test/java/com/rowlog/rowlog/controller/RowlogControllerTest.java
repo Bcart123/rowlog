70ae7a9431b84d6218ac6029a2f220d62d92e324
bcart123 <bcarthel@aggienetwork.com>
2019-11-01 00:13:15 -0500

    controller test

---
+++ src/test/java/com/rowlog/rowlog/controller/RowlogControllerTest.java
@@ -0,0 +1,16 @@
+package com.rowlog.rowlog.controller;
+
+import com.rowlog.rowlog.models.Workout;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RowlogControllerTest {
+    private RowlogController rowlogController = new RowlogController();
+
+    @Test
+    void getWorkout() {
+        Workout workout = rowlogController.getWorkout(5);
+        assertNotNull(workout);
+    }
+}
